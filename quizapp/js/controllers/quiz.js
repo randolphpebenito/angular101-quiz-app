4e957249ff1e7d5a65b7bfa215dddf43d699b1fd
Randolph Pe Benito <rpebenito@xyber.ph>
2017-04-13 00:25:27 +0800

    Added debug log.

--- quizapp/js/controllers/quiz.js
+++ quizapp/js/controllers/quiz.js
@@ -3,13 +3,82 @@
         .module('quizApp')
         .controller("quizCtrl", QuizController); /* Defining a named function instead of anonymous function */
 
-        ListController.$inject = ['quizMetrics', 'DataService']
+        QuizController.$inject = ['quizMetrics', 'DataService']
 
         function QuizController(quizMetrics, DataService) {
             var vm = this;
 
             vm.quizMetrics = quizMetrics;
             vm.dataService = DataService;
+            vm.questionAnswered = questionAnswered;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.selectAnswer = selectAnswer;
+            vm.finalizeAnswers = finalizeAnswers;
+            vm.activeQuestion = 0;
+            vm.error = false;
+            vm.finalize = false;
+
+            var numQuestionsAnswered = 0;
+
+
+            function setActiveQuestion(index) {
+                if (index === undefined) {
+                    var breakOut = false;
+                    var quizLength = DataService.quizQuestions.length - 1;
+
+                    while(!breakOut) {
+                        vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion:0;
+
+                        if (vm.activeQuestion === 0) {
+                            vm.error = true;
+                        }
+
+                        if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
+                            breakOut = true;
+                        }
+                    }
+                } else {
+                    vm.activeQuestion = index;
+                }
+
+            }
+
+            function questionAnswered() {
+
+                var quizLength = DataService.quizQuestions.length;
+
+                if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+                    numQuestionsAnswered++;
+                    if (numQuestionsAnswered >= quizLength) {
+                       //not yet 
+                       for(var i = 0; i < quizLength; i++) {
+                           if(DataService.quizQuestions[i].selected === null) {
+                               setActiveQuestion(i);
+                               return;
+                           }
+                       }
+                       vm.error = false;
+                       vm.finalize = true;
+                       return;
+                    }
+                }
+                vm.setActiveQuestion();
+
+            }
+
+            function selectAnswer(index) {
+                DataService.quizQuestions[vm.activeQuestion].selected = index; 
+            }
+
+            function finalizeAnswers() {
+                console.log("FINALIZING ANSWERS!!!");
+                vm.finalize = false;
+                numQuestionsAnswered = 0;
+                vm.activeQuestion = 0;
+                quizMetrics.markQuiz();
+                quizMetrics.changeState("quiz", false);
+                quizMetrics.changeState("results", true);
+            }
 
         }
 })();
